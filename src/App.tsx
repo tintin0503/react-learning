9018bea4bfa9c09e30901d5b40c1b86d5453ddfe
tintin0503 <quanptit2801@gmail.com>
2023-08-25 21:55:50 +0700

    change to typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  useEffect(() => {
+    console.log('Rendering <App />')
+  })
+
+  return <div className="App"></div>
+}
+
+export default App
